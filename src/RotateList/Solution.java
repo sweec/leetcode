7f10d2b43a2ac7527f66919722311a93092b6bb5
sweec <us917@yahoo.com>
2014-02-13 14:35:17 -0500

    Rotate List

---
+++ src/RotateList/Solution.java
@@ -0,0 +1,40 @@
+package RotateList;
+
+public class Solution {
+    public ListNode rotateRight(ListNode head, int n) {
+        if (head == null || head.next == null)
+        	return head;
+        ListNode preN = head, cur = head;
+        // search for node that is n nodes away from head
+        int count = 1;
+        while (count <= n) {
+        	cur = cur.next;
+        	if (cur == null)
+        		cur = head;
+        	count++;
+        }
+        // if rotates back, nothing need to do
+        if (cur == head)
+        	return head;
+        // move preN, cur until cur become last node
+        while (cur.next != null) {
+        	cur = cur.next;
+        	preN = preN.next;
+        	if (preN == null)
+        		preN = head;
+        }
+        cur.next = head;
+        head = preN.next;
+        preN.next = null;
+        return head;
+    }
+ 
+    public class ListNode {
+    	int val;
+    	ListNode next;
+    	ListNode(int x) {
+    		val = x;
+    		next = null;
+    	}
+    }
+}
---
+++ test/RotateList/SolutionTest.java
@@ -0,0 +1,49 @@
+package RotateList;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import RotateList.Solution.ListNode;
+
+public class SolutionTest {
+
+	@Test
+	public void testRotateRight() {
+		Solution s = new Solution();
+		ListNode head = s.new ListNode(1);
+		ListNode cur = head;
+		int[] data = {1, 2, 3, 4, 5};
+		for (int i = 1;i < data.length;i++) {
+			ListNode node = s.new ListNode(data[i]);
+			cur.next = node;
+			cur = node;
+		}
+		int n = 2;
+		head = s.rotateRight(head, n);
+		cur = head;
+		int[] val = {4, 5, 1, 2, 3};
+		for (int i = 1;i <= val.length;i++) {
+			assertEquals("node "+i+" value not correct", val[i-1], cur.val);
+			cur = cur.next;
+		}
+		
+		head = s.new ListNode(1);
+		cur = head;
+		int[] data2 = {1, 1, 2, 3, 3};
+		for (int i = 1;i < data2.length;i++) {
+			ListNode node = s.new ListNode(data2[i]);
+			cur.next = node;
+			cur = node;
+		}
+		n = 10;
+		head = s.rotateRight(head, n);
+		cur = head;
+		int[] val2 = {1, 1, 2, 3, 3};
+		for (int i = 1;i <= val2.length;i++) {
+			assertEquals("node "+i+" value not correct", val2[i-1], cur.val);
+			cur = cur.next;
+		}
+	}
+
+}
