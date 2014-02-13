81e0d5935ac62c9fc99d6a8ae00fecfd6a5b23b3
sweec <us917@yahoo.com>
2014-02-13 12:43:45 -0500

    Reverse Linked List II

---
+++ src/ReverseLinkedListII/Solution.java
@@ -0,0 +1,37 @@
+package ReverseLinkedListII;
+
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode pre = null, cur = head;
+        int count = 1;
+        while (count < m) {
+        	pre = cur;
+        	cur = cur.next;
+        	count++;
+        }
+        ListNode nodeM = cur;
+        ListNode next = cur.next;
+        while (count < n) {
+        	ListNode temp = next.next;
+        	next.next = cur;
+        	cur = next;
+        	next = temp;
+        	count++;
+        }
+        if (pre == null)
+        	head = cur;
+        else
+        	pre.next = cur;
+        nodeM.next = next;
+        return head;
+    }
+
+    public class ListNode {
+    	int val;
+    	ListNode next;
+    	ListNode(int x) {
+    		val = x;
+    		next = null;
+    	}
+    }
+}
---
+++ test/ReverseLinkedListII/SolutionTest.java
@@ -0,0 +1,39 @@
+package ReverseLinkedListII;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ReverseLinkedListII.Solution.ListNode;
+
+public class SolutionTest {
+
+	@Test
+	public void testReverseBetween() {
+		Solution s = new Solution();
+		ListNode head = s.new ListNode(1);
+		ListNode cur = head;
+		for (int i = 2;i <= 5;i++) {
+			ListNode node = s.new ListNode(i);
+			cur.next = node;
+			cur = node;
+		}
+		int m = 2, n = 4;
+		head = s.reverseBetween(head, m, n);
+		cur = head;
+		int[] val = {1, 4, 3, 2, 5};
+		for (int i = 1;i <= 5;i++) {
+			assertEquals("node "+i+" value not correct", val[i-1], cur.val);
+			cur = cur.next;
+		}
+		m = 1; n = 3;
+		head = s.reverseBetween(head, m, n);
+		cur = head;
+		int[] val2 = {3, 4, 1, 2, 5};
+		for (int i = 1;i <= 5;i++) {
+			assertEquals("node "+i+" value not correct", val2[i-1], cur.val);
+			cur = cur.next;
+		}
+	}
+
+}
