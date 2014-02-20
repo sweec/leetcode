ecc484d50ae0c5e7cd35948d53e6d4e810897b95
sweec <us917@yahoo.com>
2014-02-20 12:07:36 -0500

    Maximum Depth of Bianry Tree

---
+++ src/MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,16 @@
+package MaximumDepthOfBinaryTree;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+        	return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+    }
+    
+    public class TreeNode {
+    	int val;
+    	TreeNode left;
+    	TreeNode right;
+    	TreeNode(int x) { val = x; }
+    }
+}
