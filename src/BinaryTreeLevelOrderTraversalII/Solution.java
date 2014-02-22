4aefb6473933fc50dbd6f9c063ece4324df08056
sweec <us917@yahoo.com>
2014-02-22 11:09:08 -0500

    Binary Tree Level Order Traversal II

---
+++ src/BinaryTreeLevelOrderTraversalII/Solution.java
@@ -0,0 +1,41 @@
+package BinaryTreeLevelOrderTraversalII;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+    	ArrayList<ArrayList<Integer>> lot = new ArrayList<ArrayList<Integer>>();
+    	ArrayList<TreeNode> curLevel = new ArrayList<TreeNode>();
+    	curLevel.add(root);
+    	while (!curLevel.isEmpty()) {
+    		ArrayList<Integer> vals = new ArrayList<Integer>();
+    		ArrayList<TreeNode> nextLevel = new ArrayList<TreeNode>();
+    		for (TreeNode n:curLevel) {
+    			if (n != null) {
+    				vals.add(n.val);
+    				nextLevel.add(n.left);
+    				nextLevel.add(n.right);
+    			}
+    		}
+    		if (!vals.isEmpty())
+    			lot.add(vals);
+    		curLevel = nextLevel;
+    	}
+    	int a = 0, b = lot.size() - 1;
+    	while (a < b) {
+    		ArrayList<Integer> temp = lot.get(b);
+    		lot.set(b, lot.get(a));
+    		lot.set(a, temp);
+    		a++;
+    		b--;
+    	}
+    	return lot;
+    }
+    
+    public class TreeNode {
+    	int val;
+    	TreeNode left;
+    	TreeNode right;
+    	TreeNode(int x) { val = x; }
+    }
+}
---
+++ test/BinaryTreeLevelOrderTraversalII/SolutionTest.java
@@ -0,0 +1,50 @@
+package BinaryTreeLevelOrderTraversalII;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import BinaryTreeLevelOrderTraversalII.Solution.TreeNode;
+
+public class SolutionTest {
+
+	@Test
+	public void testLevelOrderBottom() {
+		Solution s = new Solution();
+		TreeNode r1 = s.new TreeNode(1);
+		r1.left = s.new TreeNode(2);
+		r1.right = s.new TreeNode(2);
+		r1.left.left = s.new TreeNode(3);
+		r1.left.right = s.new TreeNode(4);
+		r1.right.left = s.new TreeNode(4);
+		r1.right.right = s.new TreeNode(3);
+		int[][] vals = {{3, 4, 4, 3}, {2, 2}, {1}};
+		ArrayList<ArrayList<Integer>> lot = s.levelOrderBottom(r1);
+		for (int i=0;i<vals.length;i++)
+			for (int j=0;j<vals[i].length;j++)
+				assertEquals((j+1)+"th value is wrong at level "+(i+1), (Integer)vals[i][j], lot.get(i).get(j));
+		r1.left.right = r1.left.left;
+		r1.left.left = null;
+		r1.right.left = null;
+		int[][] vals2 = {{3, 3}, {2, 2}, {1}};
+		lot = s.levelOrderBottom(r1);
+		for (int i=0;i<vals2.length;i++)
+			for (int j=0;j<vals2[i].length;j++)
+				assertEquals((j+1)+"th value is wrong at level "+(i+1), (Integer)vals2[i][j], lot.get(i).get(j));
+		r1.val = 3;
+		r1.left.val = 9;
+		r1.right.val = 20;
+		r1.right.left = r1.left.right;
+		r1.right.left.val = 15;
+		r1.left.right = null;
+		r1.right.right.val = 7;
+		int[][] vals3 = {{15, 7}, {9, 20}, {3}};
+		lot = s.levelOrderBottom(r1);
+		for (int i=0;i<vals3.length;i++)
+			for (int j=0;j<vals3[i].length;j++)
+				assertEquals((j+1)+"th value is wrong at level "+(i+1), (Integer)vals3[i][j], lot.get(i).get(j));
+	}
+
+}
