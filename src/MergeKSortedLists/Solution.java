becf1816af52c05559877fd8e81f96ec8318b98a
sweec <us917@yahoo.com>
2014-03-06 22:12:18 -0500

    Merge K Sorted Lists

---
+++ src/MergeKSortedLists/Solution.java
@@ -0,0 +1,86 @@
+package MergeKSortedLists;
+
+import java.util.ArrayList;
+
+public class Solution {
+	
+	public ListNode mergeKLists(ArrayList<ListNode> lists) {
+		if (lists == null || lists.isEmpty())
+			return null;
+		int n = lists.size();
+		if (n == 1)
+			return lists.get(0);
+		ListNode[] heap = new ListNode[n+1];
+		int index = 0;
+		for (int i=0;i<n;i++) {
+			ListNode node = lists.get(i);
+			if (node != null)
+				heap[++index] = node;
+		}
+		if (index == 0)
+			return null;
+		if (index == 1)
+			return heap[1];
+		for (int i=index-1;i>0;i--)
+			rebuild(heap, i, index);
+		ListNode head = heap[1];
+		int last = index;
+		do {
+			ListNode pre = heap[1];
+			if (heap[1].next != null)
+				heap[1] = heap[1].next;
+			else {
+				heap[1] = heap[last];
+				heap[last] = null;
+				last--;
+				if (last == 1) {
+					pre.next = heap[1];
+					break;
+				}
+			}
+			rebuild(heap, 1, index);
+			pre.next = heap[1];
+		} while (true);
+		return head;
+	}
+
+	private void rebuild(ListNode[] heap, int index, int n) {
+		do {
+			ListNode p = null, l = null, r = null;
+			if (n >= index)
+				p = heap[index];
+			if (p == null)
+				break;
+			if (n >= (index << 1))
+				l = heap[index << 1];
+			if (n >= (index << 1) + 1)
+				r = heap[(index << 1) + 1];
+			if (l == null && r == null)
+				break;
+			if (l != null && (r == null || r.val >= l.val)) {
+				if (p.val > l.val) {
+					heap[index] = l;
+					heap[index << 1] = p;
+					index <<= 1;
+				} else
+					break;
+			} else if (r != null && (l == null || l.val >= r.val)) {
+				if (p.val > r.val) {
+					heap[index] = r;
+					heap[(index << 1)+1] = p;
+					index = (index << 1)+1;
+				} else
+					break;
+			}
+		} while (true);
+	}
+	
+    public class ListNode {
+    	int val;
+    	ListNode next;
+    	ListNode(int x) {
+    		val = x;
+    		next = null;
+    	}
+    }
+}
---
+++ test/MergeKSortedLists/SolutionTest.java
@@ -0,0 +1,36 @@
+package MergeKSortedLists;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import MergeKSortedLists.Solution.ListNode;
+
+public class SolutionTest {
+
+	@Test
+	public void testMergeKLists() {
+		Solution s = new Solution();
+		ArrayList<ListNode> lists = new ArrayList<ListNode>();
+		int[][] nums = {{-6,-3,-1,1,2,2,2},{-10,-8,-6,-2,4},{-2},{-8,-4,-3,-3,-2,-1,1,2,3},{-8,-6,-5,-4,-2,-2,2,4}};
+		int[] vals = {-10,-8,-8,-8,-6,-6,-6,-5,-4,-4,-3,-3,-3,-2,-2,-2,-2,-2,-1,-1,1,1,2,2,2,2,2,3,4,4};
+		for (int i=0;i<nums.length;i++) {
+			ListNode head = s.new ListNode(nums[i][0]);
+			ListNode pre = head;
+			for (int j=1;j<nums[i].length;j++) {
+				ListNode cur = s.new ListNode(nums[i][j]);
+				pre.next = cur;
+				pre = cur;
+			}
+			lists.add(head);
+		}
+		ListNode r = s.mergeKLists(lists);
+		for (int i=0;i<vals.length;i++) {
+			assertEquals("value not agree", vals[i], r.val);
+			r = r.next;
+		}
+	}
+
+}
